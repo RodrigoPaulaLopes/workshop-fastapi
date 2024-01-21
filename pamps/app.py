b2921b551982bfc857426759e4b2484da973d8f8
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2024-01-20 23:03:23 -0300

    feat: create docker compose containers

--- pamps/app.py
+++ pamps/app.py
@@ -1,3 +1,9 @@
 from fastapi import FastAPI
 
 app = FastAPI(title="Pamps", version='0.1.0', description="Pamps is a post app")
+
+
+@app.get("/")
+async def hello():
+    return {"hello": "world aaaaa"}
+
